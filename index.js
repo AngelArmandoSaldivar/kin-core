092347c188037cbbad210252366ed4eafadf6036
AngelArmandoSaldivar <ang.arm.saldivar@gmail.com>
2024-05-24 04:15:51 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const BodyParser = require("body-parser");
+const cors = require('cors');
+const { response } = require('express');
+const app = express()
+app.use(BodyParser.json());
+app.use(BodyParser.urlencoded({ extended: true }));
+
+
+app.use(cors());
+
+app.listen(3001, () => {
+    console.log("SUCCESS");
+});
+
+app.get("/kin/api/v1/echoMessage", (request, response) => {
+    response.send("Hola mundo");
+});
