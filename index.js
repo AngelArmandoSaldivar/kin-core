82b062e97f64d5e909912922aaf3ed9f0f6b46a2
AngelArmandoSaldivar <ang.arm.saldivar@gmail.com>
2024-05-27 15:10:16 -0600

    Se agregaron diferentes url para mensajes

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ const express = require('express');
 const BodyParser = require("body-parser");
 const cors = require('cors');
 const { response } = require('express');
+const config = require('./config');
 const app = express()
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
-const PORT = process.env.PORT || 5000;
 
 //app.use(cors());
 
@@ -17,7 +17,7 @@ app.get("/kin/api/v1/echoMessage", (request, response) => {
     response.send("Message Test");
 });
 
-app.post('/coreBanking/AUTH', (request, response) => {    
+app.post('/coreBanking/AUTH', (request, response) => {
 
     const reqMessage = request.body;
 
@@ -73,6 +73,6 @@ app.post('/coreBanking/ECHO', (request, response) => {
     response.send(bodyResponse);
 });
 
-app.listen(PORT, () => {
-    console.log(`Example app listening on port ${PORT}`)
+app.listen(config.PORT, config.HOST, () => {
+    console.log(`App listening on http://${config.HOST}:${config.PORT}`);
 });
