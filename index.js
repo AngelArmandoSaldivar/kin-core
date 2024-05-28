805908a8e2dab9008a39df11df603326e52a47ac
AngelArmandoSaldivar <ang.arm.saldivar@gmail.com>
2024-05-28 01:34:04 -0600

    Corección json

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 const port = process.env.PORT || 5000;
 require('dotenv').config();
+const nsrestlet = require('nsrestlet');
 
 //app.use(cors());
 
@@ -17,16 +18,42 @@ app.get("/kin/api/v1/echoMessage", (request, response) => {
 
 app.post('/coreBanking/AUTH', (request, response) => {
 
-    const reqMessage = request.body;
 
-    var bodyResponse = {
-        messageId : '',
-        validationResponse : ''
+    request = request.body;
+    var accountSettings = {
+        accountId: process.env.ACCOUNT_ID,
+        tokenKey: process.env.TOKEN_KEY,
+        tokenSecret: process.env.TOKEN_SECRET,
+        consumerKey: process.env.CONSUMER_KEY,
+        consumerSecret: process.env.CONSUMER_SECRET };
+    var urlSettings = {
+        url: process.env.URL
     }
-   
-    bodyResponse.messageId = reqMessage.messageId;
-    bodyResponse.validationResponse = "OK";
-    response.send(bodyResponse); 
+    
+    //create a link
+    var myInvoices = nsrestlet.createLink(accountSettings, urlSettings);
+
+    var messageResponse = {
+        messageId: '',
+        validationResponse:''
+    }
+    
+    //then call get, post, put, or delete
+    myInvoices.get({id: '12345'}, function(error, body)
+    {
+        // response.send(body);
+        if(request.financial_institution_id != body.financial_instituto_id || request.accountNumber != body.accountNumber || request.billingAmount != body.billingAmount) {
+            messageResponse.messageId = request.messageId;
+            messageResponse.validationResponse = "CORE_BANK_DECLINED";
+            response.send(messageResponse);
+            return;
+        } else {
+            messageResponse.messageId = request.messageId;
+            messageResponse.validationResponse = "OK";
+            response.send(messageResponse);
+        }
+        
+    });
 });
 
 app.post('/coreBanking/ADVICE', (request, response) => {    
