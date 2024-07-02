148064ac35e166c51a55db1c7973191cd65451b8
AngelArmandoSaldivar <ang.arm.saldivar@gmail.com>
2024-07-02 15:44:37 -0600

    Commit

--- index.js
+++ index.js
@@ -949,7 +949,7 @@ app.post('/app/setTimeOut', (request, response) => {
 app.get('/app/setTimeOut', (request, response) => {    
     setTimeout(() => {
         response.send(true);
-    }, request.body.time);
+    }, 50000);
 });
 
 app.listen(process.env.PORT || 5000, () => {
