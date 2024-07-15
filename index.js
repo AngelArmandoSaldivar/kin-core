e33bbfe9fafc5fd7279e8e38a7247866f7a3dcd5
AngelArmandoSaldivar <ang.arm.saldivar@gmail.com>
2024-07-15 08:11:16 -0600

    Neva funcionalidad REVADV

--- index.js
+++ index.js
@@ -1285,6 +1285,8 @@ app.post('/coreBanking/REVADV', (request, response) => {
                         }
                     }
 
+                } else {
+                    response.send("Still in development");
                 }
                                   
             }
