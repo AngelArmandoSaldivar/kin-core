d8cc871a5caecd872ef5643be5f1e8d055afe980
AngelArmandoSaldivar <ang.arm.saldivar@gmail.com>
2024-07-30 15:58:46 -0600

    Se agrego metodo para validar documentos consulta

--- index.js
+++ index.js
@@ -1599,7 +1599,7 @@ app.post('/validateDocuments/v1', (request, response) => {
 
     var body = request.body;
 
-    console.log("BODY: " + body.uuid);
+    console.log("BODY: " + JSON.stringify(body.uuid));
 
     const axios = require('axios');
     let data = JSON.stringify({
